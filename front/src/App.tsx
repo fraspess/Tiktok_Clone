9c9e24cb968085a7c89c8afa81e9d380f8a2d896
fraspess <pavlosamchyk@gmail.com>
2026-06-24 15:47:31 +0300

    Fixed frontend chadcn

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,22 +1,17 @@
-import {Route, Routes} from "react-router-dom";
-import MainLayout from "@/components/layout/MainLayout.tsx";
+import {Route, Routes} from 'react-router-dom'
 import HomePage from "@/pages/HomePage.tsx";
-import LoginPage from "@/pages/LoginPage.tsx";
-import RegisterPage from "@/pages/RegisterPage.tsx";
-
+import MainLayout from "@/components/layout/MainLayout.tsx";
 
 function App() {
 
   return (
     <>
-        <Routes>
-            <Route path="/" element={<MainLayout />} >
-                <Route index element={<HomePage/>} />
-            </Route>
+      <Routes>
+        <Route path="/" element={<MainLayout />} >
+          <Route index element={<HomePage/>} />
+        </Route>
 
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-        </Routes>
+      </Routes>
     </>
   )
 }
