7a0e1c5d6cc8bc8f291fd3f68efabdc90d514bab
fraspess <pavlosamchyk@gmail.com>
2026-08-20 01:29:10 +0300

    upload video frontend

--- front/src/components/layout/MainLayout.tsx
+++ front/src/components/layout/MainLayout.tsx
@@ -16,12 +16,15 @@ const MainLayout = ({children}: MainLayoutProps) => {
 
     return (
         <div className="flex h-screen">
-            <Sidebar
-                collapsed={isCollapsed}
-                onToggle={() => setIsCollapsed(!isCollapsed)}
-                isMessagesOpen={isMessagesOpen}
-                onMessagesClick={() => setIsMessagesOpen(true)}
-            />
+            <div>
+                <Sidebar
+                    collapsed={isCollapsed}
+                    onToggle={() => setIsCollapsed(!isCollapsed)}
+                    isMessagesOpen={isMessagesOpen}
+                    onMessagesClick={() => setIsMessagesOpen(true)}
+                />
+
+            </div>
             <div className="flex flex-col flex-1">
                 <Topbar/>
                 <main className="flex-1 overflow-hidden">
