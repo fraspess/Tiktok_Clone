91f8267ca6e84509143c0536c711d036a1d36fd8
harased <Mykola.Shamanskiy2017@gmail.com>
2026-09-24 14:42:43 +0300

    save commit before change mobile design

--- front/src/pages/HomePage.tsx
+++ front/src/pages/HomePage.tsx
@@ -1,9 +1,16 @@
 import VideoFeed from "@/components/feed/VideoFeed.tsx";
+import mascot from "@/assets/mascot-full.png";
 
 const HomePage = () => {
     return (
-        <div className="h-full w-full">
+        <div className="relative h-full w-full overflow-hidden bg-black">
             <VideoFeed/>
+
+            <img
+                src={mascot}
+                alt=""
+                className="pointer-events-none absolute -bottom-16 -right-2 z-20 w-[520px] max-w-[60vw] select-none opacity-90"
+            />
         </div>
     )
 }
